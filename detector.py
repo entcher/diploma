14cf7c9ec9d8ddfe1d1a972a412920e43c6d7ee2
entcher <49116032+entcher@users.noreply.github.com>
2024-04-11 20:40:28 +0300

    new ui and app

--- detector.py
+++ detector.py
@@ -1,21 +1,63 @@
 from human import Human
 import cv2
 import mediapipe as mp
+from formulas import *
 
 
-def put_text(image, text):
+def draw_exercises(img, exercises):
+    if not exercises:
+        return
+
     font = cv2.FONT_HERSHEY_COMPLEX
-    org = (0, 15)
-    fontScale = 0.5
-    color = (0, 0, 255)
-    thickness = 1
+    font_scale = 0.5
+    font_thickness = 1
+    text_color = (0, 255, 0)
+    text_color_bg = (0, 0, 255)
 
-    image = cv2.putText(image, text, org, font,
-                        fontScale, color, thickness, cv2.LINE_AA)
+    max_text_width = 0
+    total_text_height = 0
+
+    for ex_name, ex_count in exercises.items():
+        text = f'{ex_name}: {ex_count}'
+        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+        text_w, text_h = text_size
+        if (text_w > max_text_width):
+            max_text_width = text_w
+        total_text_height += text_h + 5
+
+        cv2.putText(img, text, (3, total_text_height),
+                    font, font_scale, text_color, font_thickness)
+
+    cv2.rectangle(img, (2, 2), (max_text_width+5,
+                  total_text_height+5), text_color_bg)
 
 
-# add isVideo param
-def show(file_path):
+def draw_finish_text(img):
+    font = cv2.FONT_HERSHEY_COMPLEX
+    font_scale = 0.5
+    font_thickness = 1
+    text_color = (0, 255, 0)
+    text_color_bg = (0, 0, 255)
+
+    text = 'План выполнен. Можете закрывать окно'
+    text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    text_w, text_h = text_size
+
+    cv2.rectangle(img, (2, 2), (text_w+2, text_h+7), text_color_bg, -1)
+    cv2.putText(img, text, (3, text_h+5), font, font_scale,
+                text_color, font_thickness)
+
+
+def convert_gui_exercises_to_simple(gui_exercises):
+    exercises = {}
+    for ex_name, ex_count in gui_exercises.items():
+        ex_count_int = int(ex_count.text())
+        if ex_count_int > 0:
+            exercises[ex_name.text()] = ex_count_int
+    return exercises
+
+
+def show(gui_exercises):
     mp_drawing = mp.solutions.drawing_utils
     mp_pose = mp.solutions.pose
 
@@ -23,20 +65,47 @@ def show(file_path):
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5)
 
-    img = cv2.imread(file_path)
-    RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = pose.process(RGB)
+    exercises = convert_gui_exercises_to_simple(gui_exercises)
+    stage = None
 
-    human = Human(results.pose_landmarks.landmark)
-    pose_name = human.estimate_pose()
-    print(f'Положение: {pose_name}')
+    cap = cv2.VideoCapture(0)
 
-    mp_drawing.draw_landmarks(
-        img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    put_text(img, pose_name)
-    cv2.imshow('Результат', img)
-    cv2.waitKey(0)
+        if not exercises:
+            draw_finish_text(frame)
+        draw_exercises(frame, exercises)
+
+        RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(RGB)
+
+        try:
+            landmarks = results.pose_landmarks.landmark
+            human = Human(landmarks)
+
+            shoulder = human.keypoints['LEFT_SHOULDER']
+            elbow = human.keypoints['LEFT_ELBOW']
+            wrist = human.keypoints['LEFT_WRIST']
+
+            angle = get_angle(shoulder, elbow, wrist)
+            print(angle)
+            if angle > 140:
+                stage = 'down'
+            elif angle < 50 and stage == 'down':
+                stage = 'up'
+                exercises['Сгибания в локтях'] -= 1
+                if exercises['Сгибания в локтях'] == 0:
+                    exercises.pop('Сгибания в локтях')
+        except:
+            pass
+
+        mp_drawing.draw_landmarks(
+            frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv2.imshow('Окно тренировки', frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
     cv2.destroyAllWindows()
-
-
--- gui.py
+++ gui.py
@@ -1,37 +1,78 @@
 from detector import show
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QFileDialog, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIntValidator, QFont
+from PyQt5.QtWidgets import QPushButton, QLabel, QLineEdit, QFormLayout, QWidget, QComboBox, QMessageBox
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Определение положения тела человека")
-        self.setGeometry(100, 100, 400, 100)
+        self.choose_label = QLabel('Выберите набор упражнений', self)
 
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout()
-        central_widget.setLayout(layout)
+        exercises_names = ['Сгибания в локтях', 'Приседания', 'Наклоны', 'Отжимания']
+        self.exercises = self.init_exercises_gui(exercises_names)
 
-        self.button = QPushButton("Выбрать файл", self)
-        layout.addWidget(self.button)
-        self.button.clicked.connect(self.open_file_dialog)
+        self.start_button = QPushButton('Начать тренировку', self)
+        self.start_button.clicked.connect(
+            lambda: self.start_workout(self.exercises))
 
-        self.file_path_label = QLabel("Выберите файл!", self)
-        layout.addWidget(self.file_path_label)
+        self.set_of_exercises_label = QLabel('Готовые комплексы упражнений')
+        self.set_of_exercises = QComboBox()
+        set_of_exercises_items = ['Свой', 'Утренний', 'Дневной', 'Ночной']
+        self.set_of_exercises.addItems(set_of_exercises_items)
+        self.set_of_exercises.currentIndexChanged.connect(self.on_set_changed)
 
-        self.done_button = QPushButton("Готово", self)
-        self.done_button.clicked.connect(self.define_pose)
-        layout.addWidget(self.done_button)
+        layout = QFormLayout()
+        layout.addWidget(self.choose_label)
+        for ex_label, ex_input in self.exercises.items():
+            layout.addRow(ex_label, ex_input)
+        layout.addWidget(self.start_button)
+        layout.addWidget(self.set_of_exercises_label)
+        layout.addWidget(self.set_of_exercises)
 
-    def open_file_dialog(self):
-        file_path, _ = QFileDialog.getOpenFileName(
-            self, "Выбрать файл", "", "Media File (*.mp4, *.jpg)")
-        if file_path:
-            self.file_path_label.setText(file_path)
+        self.setLayout(layout)
+        self.setWindowTitle('Контроль упражнений')
+        self.setGeometry(100, 100, 300, 100)
 
-    def define_pose(self):
-        if self.file_path_label.text() == 'Выберите файл!':
+    def init_exercises_gui(self, names):
+        exercises = {}
+        for name in names:
+            label = QLabel(name)
+            input = QLineEdit()
+            input.setValidator(QIntValidator(0, 2147483647))
+            input.setAlignment(Qt.AlignmentFlag.AlignRight)
+            input.setFont(QFont('Calibri', 20))
+            input.setText('0')
+            exercises[label] = input
+        return exercises
+
+    def start_workout(self, exercises):
+        if all(value.text() == '0' for value in exercises.values()):
+            QMessageBox.warning(None, 'Ошибка', 'Не выбрано ни одно упражнение')
             return
-        show(self.file_path_label.text())
+        show(exercises)
+
+    def on_set_changed(self):
+        selected_set = self.set_of_exercises.currentText()
+        match selected_set:
+            case 'Свой':
+                self.set_exercises()
+            case 'Утренний':
+                self.set_exercises(squats=10, bends=10, pushups=5)
+            case 'Дневной':
+                self.set_exercises(squats=20, bends=10, pushups=10)
+            case 'Ночной':
+                self.set_exercises(squats=2, bends=10)
+
+    def set_exercises(self, curls=0, squats=0, bends=0, pushups=0):
+        for ex_label, ex_input in self.exercises.items():
+            match ex_label.text():
+                case 'Сгибания в локтях':
+                    ex_input.setText(str(curls))
+                case 'Приседания':
+                    ex_input.setText(str(squats))
+                case 'Наклоны':
+                    ex_input.setText(str(bends))
+                case 'Отжимания':
+                    ex_input.setText(str(pushups))
